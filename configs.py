27ced1cd989157abdad1fafcd46f0a2b3b1d88b1
tilakbaserock <tilak@baserock.ai>
2025-03-16 23:32:39 +0530

    fixed path

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
 use_cache = True
 model_name = 'bert-base-nli-mean-tokens'
-cache_base_path = "/Users/guptatilak/Documents/CoeuSearch/cache"
+cache_base_path = "/Users/guptatilak/Documents/NeuralFileSearchEngine/cache"
 probs = {'title_prob_thres':0.80, 'content_prob_thres':0.70, 'topic_prob_thres':0.75}
